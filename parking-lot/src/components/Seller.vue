289d69d745c760bf20c2ebf52e50724bbbb7b1ff
mmz <372461239@qq.com>
2020-11-25 16:45:53 +0800

    柱状图完成

--- parking-lot/src/components/Seller.vue
+++ parking-lot/src/components/Seller.vue
@@ -20,15 +20,107 @@ export default {
   mounted () {
     this.initChart()
     this.getData()
+    window.addEventListener('resize', this.screenAdapter)
+    this.screenAdapter()
   },
   destroyed () {
     clearInterval(this.timerId)
+    window.removeEventListener('resize', this.screenAdapter)
   },
   methods: {
+    screenAdapter () {
+      const titleFontsize = this.$refs.seller.offsetWidth / 100 * 3.6
+      const adapterOption = {
+        title: {
+          textStyle: {
+            fontSize: 66
+          }
+        },
+        tooltip: {
+          axisPointer: {
+            lineStyle: {
+              width: titleFontsize
+            }
+          }
+        },
+        series: [
+          {
+            barWidth: titleFontsize,
+            itemStyle: {
+              barBorderRadius: [0, titleFontsize / 2, titleFontsize / 2, 0],
+            }
+          }
+        ]
+      }
+      this.chartInstance.setOption(adapterOption)
+      this.chartInstance.resize()
+    },
+
     // 初始化echartsInstance对象
     initChart () {
       const chart = this.$refs.seller
       this.chartInstance = this.$echarts.init(chart, 'chalk')
+      const initOption = {
+        title: {
+          text: '   车场车位使用状况',
+          textStyle: {
+            fontSize: 66
+          },
+          left: 20,
+          top: 20
+        },
+        grid: {
+          top: '20%',
+          left: '3%',
+          right: '6%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'value'
+        },
+        yAxis: {
+          type: 'category',
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'line',
+            z: 0,
+            lineStyle: {
+              width: 66,
+              color: '#2D3443'
+            }
+          }
+        },
+        series: [
+          {
+            type: 'bar',
+            barWidth: 66,
+            label: {
+              show: true,
+              position: 'right',
+              textStyle: {
+                color: 'white'
+              }
+            },
+            itemStyle: {
+              barBorderRadius: [0, 33, 33, 0],
+              color: new this.$echarts.graphic.LinearGradient(0, 0, 1, 0, [
+                {
+                  offset: 0,
+                  color: '#5052EE'
+                },
+                {
+                  offset: 1,
+                  color: '#AB6ee5'
+                }
+              ])
+            }
+          }
+        ]
+      }
+      this.chartInstance.setOption(initOption)
       // 对图表对象进行鼠标监听
       this.chartInstance.on('mouseover', () => {
         clearInterval(this.timerId)
@@ -62,22 +154,17 @@ export default {
       const sellerValues = showData.map((item) => {
         return item.value
       })
-      const option = {
-        xAxis: {
-          type: 'value'
-        },
+      const dataOption = {
         yAxis: {
-          type: 'category',
           data: sellerNames
         },
         series: [
           {
-            type: 'bar',
             data: sellerValues
           }
         ]
       }
-      this.chartInstance.setOption(option)
+      this.chartInstance.setOption(dataOption)
     },
     // 定时器
     startInterval () {
